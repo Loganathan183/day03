871d14db55350885e1993b57b6713d0d7bc82718
Loganathan183 <viratlogu183@gmail.com>
2024-01-27 12:26:23 +0530

    js.

--- script1.js
+++ script1.js
@@ -3,13 +3,11 @@ remoteJSON = {"name" : "person1","age" : 5},
     localJSON = {"age" : 5,"name" :"person1"};
 console.log((remoteJSON, localJSON));
 
-
 let obj1 = {"name": "Person1", "age": "5"};
 let obj2 = {"age": "5", "name": "Person1"};
 
 const stringifiedObj1 = JSON.stringify(obj1);
 const stringifiedObj2 = JSON.stringify(obj2);
-
 if (stringifiedObj1 === stringifiedObj2) {
     console.log("The JSON objects are equal");
 } else {
